3ca6b38082edd06c049166a4695e9b18d12746b3
Wheels_Aiden <xaiden91@gmail.com>
2025-09-09 10:50:37 -0400

    Without \n

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -21,6 +21,10 @@ print("The itsy bitsy spider\nclimbed up the waterspout.")
 print()
 print("Down came the rain\nand washed the spider out.")
 
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain\nand washed the spider out.")
+
 
 
 
