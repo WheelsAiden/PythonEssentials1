5abd71a70d87b3876030e41b3c75b564d34bc15e
Wheels_Aiden <xaiden91@gmail.com>
2025-09-09 10:42:42 -0400

    Explaining different ways of spacing and basics of what (print) means.

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -4,3 +4,24 @@ print("Who is your day?")
 print("What is your favorite color?")
 
 print("I love red flowers")
+
+
+print("Aiden Walker - 9/9/25")
+
+print("The itsy bitsy spider climbed up the waterspout.")
+print("Down came the rain and washed the spider out.")
+
+
+print("The itsy bitsy spider climbed up the waterspout.")
+print()
+print("Down came the rain and washed the spider out.")
+
+
+
+
+
+
+
+
+
+
