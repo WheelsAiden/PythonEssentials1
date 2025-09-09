7ca3f7891a566fe09337dc77408df73e855676cb
Wheels_Aiden <xaiden91@gmail.com>
2025-09-09 11:17:51 -0400

    Another way of spacing out words.

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -41,7 +41,8 @@ print("Monty Python.")
 print("My", "name", "is", "Monty", "Python.", sep="-")
 
 
-
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
 
 
 
