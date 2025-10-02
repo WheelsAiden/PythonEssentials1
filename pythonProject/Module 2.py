99181f35b8dd4c153cba127d94cf02e2f3951e49
Wheels_Aiden <xaiden91@gmail.com>
2025-10-02 10:41:47 -0400

    2.4.1.4 Variables - data-shaped boxes

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -372,8 +372,18 @@ print((2 % -4), (2 % 4), (2 ** 3 ** 2))
 
 print()
 
+var = 1
+print(var)
 
+print()
 
+var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(var, account_balance, client_name)
+print(var)
+
+print()
 
 
 
