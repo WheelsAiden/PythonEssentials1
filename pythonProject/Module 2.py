33d188f1dedcade09c8d97e71fa5cc0cb6e7ea5b
Wheels_Aiden <xaiden91@gmail.com>
2025-10-06 10:50:44 -0400

    2.4.1.9 LAB: Variables: a simple converter

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -450,8 +450,17 @@ print()
 
 print("x = x ** 2 ⇒ x **= 2")
 
+print()
+print()
 
+print("""kilometers = 12.25 - miles = 7.38
 
+miles_to_kilometers = ###
+kilometers_to_miles = ###
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+""")
 
 
 
