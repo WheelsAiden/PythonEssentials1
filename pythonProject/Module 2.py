d929d6586585e16e4aaa11b054d127506acb6976
Wheels_Aiden <xaiden91@gmail.com>
2025-10-13 11:07:42 -0400

    2.6.1.2 How to talk to a computer/The input() function with an argument

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -624,8 +624,14 @@ print("Hmm...", anything, "... Really?")
 
 print()
 
+anything = input("Tell me anything...")
+print("Hmm...", anything, "...Really?")
 
+print()
 
+anything = input("Enter a number: ")
+something = anything ** 2.0
+print(anything, "to the power of 2 is", something)
 
 
 
