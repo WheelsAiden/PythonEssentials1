598d5b3fc5acca72ec012735531472b0d7553086
Wheels_Aiden <xaiden91@gmail.com>
2025-10-14 11:03:26 -0400

    Trying to figure out why all code is not being ran.

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -629,7 +629,7 @@ print("Hmm...", anything, "...Really?")
 
 print()
 
-anything = input("Enter a number: ")
+anything = float(input("Enter a number: "))
 something = anything ** 2.0
 print(anything, "to the power of 2 is", something)
 
