0c5d6bcf342e6ebad4fa8bc778f2298133729491
Wheels_Aiden <xaiden91@gmail.com>
2025-10-15 11:29:17 -0400

    2.6.1.5 - More about input() and type casting

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -650,3 +650,17 @@ anything = float(input("Enter a number: "))
 something = anything ** 2.0
 print(anything, "to the power of 2 is", something)
 
+print()
+
+print("V1")
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+hypo = (leg_a**2 + leg_b**2) ** .5
+print("Hypotenuse length is", hypo)
+
+print()
+
+print("V2")
+leg_a = float(input("Input first leg length: "))
+leg_b = float(input("Input second leg length: "))
+print("Hypotenuse length is", (leg_a**2 + leg_b**2) ** .5)
