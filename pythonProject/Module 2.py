c280846facdf5835e0dba9d078f276aeb4406fe1
Wheels_Aiden <xaiden91@gmail.com>
2025-10-15 11:37:47 -0400

    2.6.1.6 - String Operators

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -664,3 +664,14 @@ print("V2")
 leg_a = float(input("Input first leg length: "))
 leg_b = float(input("Input second leg length: "))
 print("Hypotenuse length is", (leg_a**2 + leg_b**2) ** .5)
+
+
+print()
+
+fnam = input("May I have your first name, please? ")
+lnam = input("May I have your last name, please? ")
+print("Thank you.")
+print("\nYour name is " + fnam + " " + lnam + ".")
+
+
+
