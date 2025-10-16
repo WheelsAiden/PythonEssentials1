05d2cc51e61bdf270cb8172327e0292449fdeca3
Wheels_Aiden <xaiden91@gmail.com>
2025-10-16 10:35:20 -0400

    2.6.1.12 SECTION SUMMARY

--- pythonProject/Module 2.py
+++ pythonProject/Module 2.py
@@ -813,3 +813,33 @@ print("1")
 print("2939")
 print("Expected output: 1:0")
 
+print()
+
+name = input("Enter your name: ")
+print("Hello, " + name + ". Nice to meet you!")
+
+print("\nPress Enter to end the program.")
+input()
+print("THE END.")
+
+print()
+
+num_1 = input("Enter the first number: ") # Enter 12
+num_2 = input("Enter the second number: ") # Enter 21
+
+print(num_1 + num_2) # the program returns 1221
+
+print()
+
+my_input = input("Enter something: ") # Example input: hello
+print(my_input * 3) # Expected output: hellohellohello
+
+print()
+
+x = int(input("Enter a number: ")) # The user enters 2
+print(x * "5")
+
+print()
+
+x = input("Enter a number: ") # The user enters 2
+print(type(x))
