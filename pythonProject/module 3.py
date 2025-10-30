e9da3c59225ea29a404fb4c62cb8ba58d26a7b5d
Wheels_Aiden <xaiden91@gmail.com>
2025-10-30 11:01:13 -0400

    3.1.1.1 Comparison: equality operator

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -1,3 +1,24 @@
 print("Aiden Walker - 10/30/25")
 print ()
 print("Aiden is the best student of the class. I work very hard everyday and I love my 3rd block.")
+
+print()
+
+print("Aiden Walker - 10/30/25")
+print()
+
+# Question 1
+print(2 == 2)
+
+# Question 2
+print(2 == 2.0)
+
+# Question 3
+print(1 == 2)
+
+# Making my own questions
+print(100==50)
+
+print(100==100)
+
+print()
