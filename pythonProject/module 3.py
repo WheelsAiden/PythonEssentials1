be43d79009a216d5cbdb4d76bbbc8ebe7a2fa73f
Wheels_Aiden <xaiden91@gmail.com>
2025-11-03 10:53:44 -0500

    3.1.1.2 Equality: the equal to operator (==)

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -1,3 +1,5 @@
+from idlelib.configdialog import VarTrace
+
 print("Aiden Walker - 10/30/25")
 print ()
 print("Aiden is the best student of the class. I work very hard everyday and I love my 3rd block.")
@@ -22,3 +24,32 @@ print(100==50)
 print(100==100)
 
 print()
+
+print("Aiden Walker - 11/3/25")
+
+print()
+
+print("var == 0")
+print()
+
+print("black_sheep == 2 * white_sheep")
+print()
+
+print("black_sheep == (2 * white_sheep")
+print()
+
+print("var = 0  # Assigning 0 to var")
+print("var == 0")
+
+print("var = 1  # Assigning 1 to var")
+print("var == 0")
+
+print()
+
+v = 0
+a = 0
+
+print("v + a =", v + a)
+
+
+
