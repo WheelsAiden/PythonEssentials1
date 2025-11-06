692870ff80288836603d965e2b4b625c0f11edd5
Wheels_Aiden <xaiden91@gmail.com>
2025-11-06 10:35:24 -0500

    3.1.1.8 Analyzing code samples/ comment out stuff

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -1,3 +1,4 @@
+"""
 from idlelib.configdialog import VarTrace
 
 print("Aiden Walker - 10/30/25")
@@ -124,3 +125,63 @@ print("Aiden Walker - 11/5/25")
 print()
 
 print("No coding yet — I’m just reviewing if, elif, else statements. Back when I was a freshman, I learned about them and even made a repo for it, but I can’t access it now because my school’s blocking system won’t let me.")
+
+print()
+"""
+print("Aiden Walker - 11/6/25")
+
+print()
+
+print("Example 1")
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2:
+    larger_number = number1
+else:
+    larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
+
+print()
+
+print("Example 2")
+# Read two numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+# Choose the larger number
+if number1 > number2: larger_number = number1
+else: larger_number = number2
+
+# Print the result
+print("The larger number is:", larger_number)
+
+print()
+
+print("Example 3")
+# Read three numbers
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+# We temporarily assume that the first number
+# is the largest one.
+# We will verify this soon.
+largest_number = number1
+
+# We check if the second number is larger than current largest_number
+# and update largest_number if needed.
+if number2 > largest_number:
+    largest_number = number2
+
+# We check if the third number is larger than current largest_number
+# and update largest_number if needed.
+if number3 > largest_number:
+    largest_number = number3
+
+# Print the result
+print("The largest number is:", largest_number)
