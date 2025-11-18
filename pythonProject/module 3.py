7ce3983281778c7b81995355a8cad9092565f807
Wheels_Aiden <xaiden91@gmail.com>
2025-11-18 11:06:01 -0500

    3.2.1.4 Loops in Python | for

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -495,4 +495,15 @@ print()
 
 print("Well done, muggle! You are free now.")
 
+print()
 
+i = 0
+while i < 100:
+    # do_something()
+    i += 1
+
+print()
+
+for i in range(100):
+    # do_something()
+    pass
