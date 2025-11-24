cd63a789b3e3782b4650a3af147de3d34815dca6
Wheels_Aiden <xaiden91@gmail.com>
2025-11-24 10:33:59 -0500

    3.2.1.5 Loops in Python | for

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -517,3 +517,14 @@ print()
 
 for i in range(2, 8):
     print("The value of i is currently", i)
+
+print()
+
+print("Aiden Walker - 11/24/2025")
+
+print()
+
+power = 1
+for expo in range(16):
+    print("2 to the power of", expo, "is", power)
+    power *= 2
