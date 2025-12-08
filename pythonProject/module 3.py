aa30e2fd8fbeaffbbb4922c9dbaa0607d227daef
Wheels_Aiden <xaiden91@gmail.com>
2025-12-08 10:39:13 -0500

    3.2.1.13 The for loop and the else branch

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -679,3 +679,20 @@ else:
     print("else:", i)
 
 print()
+
+#1st part
+i = 111
+for i in range(2, 1):
+    print(i)
+else:
+    print("else:", i)
+
+print()
+
+#2nd part
+for i in range(5):
+    print(i)
+else:
+    print("else:", i)
+
+print()
