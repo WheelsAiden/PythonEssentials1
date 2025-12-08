61a9581c230b5119be5d1af372d6d00b149ef658
Wheels_Aiden <xaiden91@gmail.com>
2025-12-08 10:42:07 -0500

    3.2.1.14 LAB: Essentials of the while loop

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -696,3 +696,15 @@ else:
     print("else:", i)
 
 print()
+
+blocks = int(input("Enter the number of blocks: "))
+
+height = 0
+layer = 1
+
+while blocks >= layer:
+    blocks -= layer
+    height += 1
+    layer += 1
+
+print("The height of the pyramid:", height)
