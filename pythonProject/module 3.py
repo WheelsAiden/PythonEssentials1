4fe9ccf1f5b075ccce28f15c907a9cb49f66976a
Wheels_Aiden <xaiden91@gmail.com>
2025-12-11 10:27:15 -0500

    3.3.1.6 SECTION SUMMARY

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -896,4 +896,35 @@ var_right = var >> 1
 var_left = var << 2
 print(var, var_left, var_right)
 
+print()
+
+print("Aiden Walker - 12/11/25")
+
+print()
+
+# -----------------------------
+# Exercise 1
+# -----------------------------
+x = 1
+y = 0
+
+z = ((x == y) and (x == y)) or not(x == y)
+print("Exercise 1 Output:", not(z))
+
+
+# -----------------------------
+# Exercise 2
+# -----------------------------
+x = 4
+y = 1
+
+a = x & y
+b = x | y
+c = ~x      # tricky!
+d = x ^ 5
+e = x >> 2
+f = x << 2
+
+print("Exercise 2 Outputs:", a, b, c, d, e, f)
+
 
