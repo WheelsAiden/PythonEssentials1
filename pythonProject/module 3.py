48106b2d1fbf5901f321c13f01f2cf9afebdb23b
Wheels_Aiden <xaiden91@gmail.com>
2025-12-22 13:47:10 -0500

    3.4.1.6 LAB: The basics of lists

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -987,3 +987,18 @@ numbers = [111, 7, 2, 1]
 print(numbers[-1])
 print(numbers[-2])
 
+print()
+
+hat_list = [1, 2, 3, 4, 5]  # This is an existing list of numbers hidden in the hat.
+
+# Step 1: replace the middle number with an integer entered by the user
+hat_list[2] = int(input("Enter a number: "))
+
+# Step 2: remove the last element from the list
+hat_list.pop()
+
+# Step 3: print the length of the existing list
+print(len(hat_list))
+
+print(hat_list)
+
