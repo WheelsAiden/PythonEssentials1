f52fe4b8b1e970988f3657f6e32b4fc6e9887d7d
Wheels_Aiden <xaiden91@gmail.com>
2025-12-22 13:56:24 -0500

    3.4.1.10 Making use of lists

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -1040,3 +1040,26 @@ for i in range(5):
 
 print(my_list)
 
+print()
+
+#Version 1: Using indices with range() and len()
+my_list = [10, 1, 8, 3, 5]
+total = 0
+
+for i in range(len(my_list)):
+    total += my_list[i]
+
+print(total)
+
+print()
+
+#Version 2: Looping directly through the list (preferred)
+my_list = [10, 1, 8, 3, 5]
+total = 0
+
+for i in my_list:
+    total += i
+
+print(total)
+
+print()
