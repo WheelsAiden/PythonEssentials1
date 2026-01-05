42602119b41046191c12cfd92e82cdca12517337
Wheels_Aiden <xaiden91@gmail.com>
2026-01-05 10:34:13 -0500

    3.5.1.4 SECTION SUMMARY

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -1150,3 +1150,50 @@ while swapped:
 print("\nSorted:")
 print(my_list)
 
+print()
+
+lst = [5, 3, 1, 2, 4]
+print(lst)
+
+lst.sort()
+print(lst)  # outputs: [1, 2, 3, 4, 5]
+
+print()
+
+lst = [5, 3, 1, 2, 4]
+print(lst)
+
+lst.reverse()
+print(lst)  # outputs: [4, 2, 1, 3, 5]
+
+print()
+
+lst = ["D", "F", "A", "Z"]
+lst.sort()
+
+print(lst)
+
+print()
+
+a = 3
+b = 1
+c = 2
+
+lst = [a, c, b]
+lst.sort()
+
+print(lst)
+
+print()
+
+a = "A"
+b = "B"
+c = "C"
+d = " "
+
+lst = [a, b, c, d]
+lst.reverse()
+
+print(lst)
+
+print()
