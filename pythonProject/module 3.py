abac3fbae067d008f56b96bba418a66ea21cdcf2
Wheels_Aiden <xaiden91@gmail.com>
2026-01-07 10:48:57 -0500

    3.7.1.3 Lists in lists: two-dimensional arrays - continued

--- pythonProject/module 3.py
+++ pythonProject/module 3.py
@@ -1544,3 +1544,27 @@ print(board)
 board = [[EMPTY for i in range(8)] for j in range(8)]
 
 print(board)
+
+print()
+
+EMPTY = "-"
+ROOK = "ROOK"
+KNIGHT = "KNIGHT"
+PAWN = "PAWN"
+
+board = []
+
+for i in range(8):
+    row = [EMPTY for i in range(8)]
+    board.append(row)
+
+board[0][0] = ROOK
+board[0][7] = ROOK
+board[7][0] = ROOK
+board[7][7] = ROOK
+
+board[4][2] = KNIGHT
+board[3][4] = PAWN
+
+for row in board:
+    print(row)
