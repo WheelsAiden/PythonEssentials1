036e03a6df99e24ed36edd1820b0a3639b2cc025
Wheels_Aiden <xaiden91@gmail.com>
2026-01-12 10:27:07 -0500

    4.1.1.3 Writing functions part 1

---
+++ pythonProject/Mod 4.py
@@ -0,0 +1,11 @@
+print("Aiden Walker - 1/12/26")
+
+print()
+
+def enter_value():
+    print("Please, enter a value: ")
+    return int(input())
+
+a = enter_value()
+b = enter_value()
+c = enter_value()
