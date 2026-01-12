566f687294adb837eb9fe331677aaa7f6ac04d4a
Wheels_Aiden <xaiden91@gmail.com>
2026-01-12 10:31:56 -0500

    4.1.1.4 Writing functions part 2

--- pythonProject/Mod 4.py
+++ pythonProject/Mod 4.py
@@ -9,3 +9,19 @@ def enter_value():
 a = enter_value()
 b = enter_value()
 c = enter_value()
+
+print()
+
+def message():
+    print("Enter a value: ")
+
+def my_function():
+    print("Hello, this is my function!")
+
+print("We start here.")
+message()
+my_function()
+print("We end here.")
+
+print()
+
