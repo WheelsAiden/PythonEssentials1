50e79441995abf70771f0fa9fdd1d776b8bfa311
Wheels_Aiden <xaiden91@gmail.com>
2026-01-19 17:53:16 -0500

    4.3.1.9 LAB: Prime numbers - how to find them

--- pythonProject/Mod 4.py
+++ pythonProject/Mod 4.py
@@ -620,3 +620,21 @@ print(day_of_year(2024, 13, 1))    # None
 
 print()
 
+def is_prime(num):
+    if num <= 1:
+        return False
+
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+
+    return True
+
+
+for i in range(1, 20):
+    if is_prime(i + 1):
+        print(i + 1, end=" ")
+print()
+
+print()
+
