a98f38aa95a0cd5507e7a454ef6000c14c788e7a
Wheels_Aiden <xaiden91@gmail.com>
2026-01-19 17:47:11 -0500

    4.3.1.6 LAB: A leap year: writing your own functions

--- pythonProject/Mod 4.py
+++ pythonProject/Mod 4.py
@@ -484,3 +484,28 @@ print(strange_list_fun(5))
 # - Assigning the returned list copies the reference, not the contents
 #   (use copy(), slicing [:], or list() to make a real copy)
 
+print()
+
+def is_year_leap(year):
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    return False
+
+
+test_data = [1900, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+
+for i in range(len(test_data)):
+    yr = test_data[i]
+    print(yr, "->", end="")
+    result = is_year_leap(yr)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Failed")
+
+print()
