f374f60dbb9cff40cac7488a39c5cd8cb6735eaa
Wheels_Aiden <xaiden91@gmail.com>
2026-01-21 11:02:57 -0500

    4.5.1.7 Some simple functions: Fibonacci numbers

--- pythonProject/Mod 4.py
+++ pythonProject/Mod 4.py
@@ -1101,3 +1101,25 @@ def factorial_function(n):
 for n in range(1, 6):
     print(n, factorial_function(n))
 
+print()
+
+def fib(n):
+    if n < 1:
+        return None
+    if n < 3:
+        return 1
+
+    elem_1 = elem_2 = 1
+    the_sum = 0
+
+    for i in range(3, n + 1):
+        the_sum = elem_1 + elem_2
+        elem_1, elem_2 = elem_2, the_sum
+
+    return the_sum
+
+
+# testing
+for n in range(1, 10):
+    print(n, "->", fib(n))
+
