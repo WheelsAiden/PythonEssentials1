2996d4de0f33932059855a2ee953027d5ecf3f51
Wheels_Aiden <xaiden91@gmail.com>
2026-01-21 17:58:23 -0500

    4.6.1.12 SECTION SUMMARY + Key takeaways: tuples and dictionaries

--- pythonProject/Mod 4.py
+++ pythonProject/Mod 4.py
@@ -1648,3 +1648,63 @@ pol_eng_dictionary = {
 }
 
 copy_dictionary = pol_eng_dictionary.copy()
+
+print()
+
+# Exercise 1
+my_tup = (1, 2, 3)
+print(my_tup[2])  # Output: 3
+
+
+# Exercise 2
+tup = 1, 2, 3
+a, b, c = tup
+print(a * b * c)  # Output: 6
+
+
+# Exercise 3
+tup = 1, 2, 3, 2, 4, 5, 6, 2, 7, 2, 8, 9
+duplicates = tup.count(2)
+print(duplicates)  # Output: 4
+
+
+# Exercise 4
+d1 = {'Adam Smith': 'A', 'Judy Paxton': 'B+'}
+d2 = {'Mary Louis': 'A', 'Patrick White': 'C'}
+d3 = {}
+
+for item in (d1, d2):
+    d3.update(item)
+
+print(d3)
+
+
+# Exercise 5
+my_list = ["car", "Ford", "flower", "Tulip"]
+t = tuple(my_list)
+print(t)
+
+
+# Exercise 6
+colors = (("green", "#008000"), ("blue", "#0000FF"))
+colors_dictionary = dict(colors)
+print(colors_dictionary)
+
+
+# Exercise 7
+my_dictionary = {"A": 1, "B": 2}
+copy_my_dictionary = my_dictionary.copy()
+my_dictionary.clear()
+print(copy_my_dictionary)
+
+
+# Exercise 8
+colors = {
+    "white": (255, 255, 255),
+    "grey": (128, 128, 128),
+    "red": (255, 0, 0),
+    "green": (0, 128, 0)
+}
+
+for col, rgb in colors.items():
+    print(col, ":", rgb)
