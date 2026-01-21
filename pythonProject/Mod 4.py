91b92272880c4205a587120180d1295a4e24ab72
Wheels_Aiden <xaiden91@gmail.com>
2026-01-21 10:42:42 -0500

    4.5.1.2 Some simple functions: evaluating BMI and converting imperial units to metric units

--- pythonProject/Mod 4.py
+++ pythonProject/Mod 4.py
@@ -948,3 +948,28 @@ def bmi(weight, height):
 print(bmi(52.5, 1.65))   # valid values
 print(bmi(-10, 1.65))   # invalid weight
 print(bmi(52.5, 0))     # invalid height
+
+print()
+
+def ft_and_inch_to_m(ft, inch=0.0):
+    return ft * 0.3048 + inch * 0.0254
+
+
+def lb_to_kg(lb):
+    return lb * 0.45359237
+
+
+def bmi(weight, height):
+    if height < 1.0 or height > 2.5 or \
+       weight < 20 or weight > 200:
+        return None
+
+    return weight / height ** 2
+
+
+# Test: BMI for a person 5'7" tall and 176 lb
+print(bmi(weight=lb_to_kg(176), height=ft_and_inch_to_m(5, 7)))
+
+# Test: protection against invalid values
+print(bmi(352.5, 1.65))
+
