c3c2dc0b463321caea1e4304fdbb263ec9dd8a7e
Wheels_Aiden <xaiden91@gmail.com>
2026-01-21 11:01:00 -0500

    4.5.1.6 Some simple functions: factorials

--- pythonProject/Mod 4.py
+++ pythonProject/Mod 4.py
@@ -1083,3 +1083,21 @@ if is_a_triangle(a, b, c):
 else:
     print("No, it can't be a triangle.")
 
+print()
+
+def factorial_function(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+
+    product = 1
+    for i in range(2, n + 1):
+        product *= i
+    return product
+
+
+# testing
+for n in range(1, 6):
+    print(n, factorial_function(n))
+
